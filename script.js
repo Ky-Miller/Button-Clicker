e64697a248b64bf5e699970bd567af8b7f9fd8bc
Ky-Miller <kychr.miller@gmail.com>
2023-05-15 18:02:28 -0700

    1st commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function Login(element) {
+    if (element.innerText = "Login"){
+        element.innerText = "Logout";
+    }
+}
+// ^^ changes button text from Login to Logout when clickedA ^^
+function remove(element){
+    element.remove(element);
+}
+// ^^ removes the "add definition" button ^^
